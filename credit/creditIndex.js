68e776575272b27b43bca0a740766bc544d59893
RicardoAlonzo <alonzoricardo6@gmail.com>
2018-11-09 11:08:45 +0100

    separated services and included a queue with kue

---
+++ credit/creditIndex.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const express = require("express");
+const queue = require('./src/queue/queue')
+const bodyParser = require("body-parser");
+const {
+  Validator,
+  ValidationError
+} = require("express-json-validator-middleware");
+
+const updateCredit = require("./src/controllers/updateCredit");
+
+const app = express();
+
+const validator = new Validator({ allErrors: true });
+const { validate } = validator;
+
+
+const creditSchema = {
+  type: "object",
+  required: ["amount"],
+  properties: {
+    location: {
+      type: "string"
+    },
+    amount: {
+      type: "number"
+    }
+  }
+};
+
+
+app.post(
+  "/credit",
+  bodyParser.json(),
+  validate({ body: creditSchema }),
+  updateCredit
+);
+
+app.use(function(err, req, res, next) {
+  console.log(res.body);
+  if (err instanceof ValidationError) {
+    res.sendStatus(400);
+  } else {
+    res.sendStatus(500);
+  }
+});
+
+app.listen(9008, function() {
+  console.log("CREDIT CONTAINER started on PORT 9008");
+});
---
+++ message/src/queue/queue.js
@@ -0,0 +1,32 @@
+const kue = require("kue");
+const uuidv4 = require("uuid/v4");
+
+let queue = kue.createQueue();
+//const sendMessage = require("../controllers/sendMessage");
+//1
+module.exports = function(req, res) {
+  let uniqueId = uuidv4();
+  let messObj = req.body;
+  messObj.uuid = uniqueId;
+  let job = queue
+    .create("msg", {
+      messObj
+    })
+    .ttl(6000)
+    .save(function(err) {
+      if (!err) res.send(`0--> queue entry number: ${job.id}`);
+    });
+};
+
+queue.on("job enqueue", function(id, type) {
+  console.log("Job %s got queued of type %s", id, type);
+});
+
+/* queue.on('send message', function(job, done) {
+  console.log('sending message');
+}) */
+
+/* queue.process("msg", function(job, done) {
+  console.log("entra");
+  done();
+});  */
